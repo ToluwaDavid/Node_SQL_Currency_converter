daaae50decf6f6676093e1296c191b82fa943864
ToluwaDavid <mailtolulopedavid@gmail.com>
2022-11-04 23:02:42 +0100

    updating db.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express'); //To create our server.
 const cors = require("cors"); //To allow and redirect request resources.
 const routes = require("./controllers/routes/routes"); //This is where our API routes will be defined
-
+const conn = require('./services/db')
 
 const app = express();
 const port = process.env.PORT || 5000
 
 app.use('/', routes)
+
+pool
 
 app.listen(port, () => { console.log(`Listening on port ${port}`) })
---
+++ services/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+conn.connect();
+
+module.exports = conn;
