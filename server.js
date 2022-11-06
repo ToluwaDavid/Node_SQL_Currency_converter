0496fc9a1a8c26d53f6daeda622cb75c0f9ec6c9
ToluwaDavid <mailtolulopedavid@gmail.com>
2022-11-06 17:23:35 +0100

    updated server.js

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express'); //To create our server.
 const cors = require("cors"); //To allow and redirect request resources.
 const routes = require("./controllers/routes/routes"); //This is where our API routes will be defined
 const conn = require('./services/db');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000
 
-require('dotenv').config();
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(cors());
 
 app.use('/', routes)
+
+
+
 
 
 
